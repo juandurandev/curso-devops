e5599765aca133df2a07a9eeecd6126f8a4d4ec1
Juan Duran <juandurandev@gmail.com>
2026-02-19 01:51:00 -0400

    Cambios a mi repositorio

--- ContactoAPI/obj/Debug/net10.0/ContactoAPI.AssemblyInfo.cs
+++ ContactoAPI/obj/Debug/net10.0/ContactoAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d7ea1ef50851e4396457bea9ce7d76a34b1426")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
